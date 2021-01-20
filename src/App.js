ab1b324082aadf140fee50cae4977ffeb7abcf69
Your Name <you@example.com>
2021-01-19 22:20:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            dictionary: null,
+        }
+        this.fetchDictionary = this.fetchDictionary.bind(this)
+    }
+
+    fetchDictionary(e) {
+        e.preventDefault()
+        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${e.target.query.value}`)
+            .then(response => response.json())
+            .then(data => this.setState({ dictionary: data }))
+        e.target.reset()
+    }
+
+    render() {
+        const dictionary = this.state.dictionary
+        try {
+            return (
+                <div>
+                    <form onSubmit={this.fetchDictionary} id="dictionary-search">
+                        <input className="query" type="text" name="query" placeholder="Enter word..." />
+                        <input className="submit" type="submit" placeholder="Submit!" />
+                    </form>
+                    {dictionary &&
+                        <div className="results">
+                            <div className="word-info">
+                                <h1 className="word">{dictionary[0]["word"]}</h1>
+                                <h4 className="pronunciation">[ {dictionary[0]["phonetics"][0]["text"]} ]</h4>
+                            </div>
+                            <div className="meanings">
+                                {dictionary[0]["meanings"].map((meaning) => {
+                                    return (
+                                        <div className="meaning">
+                                            <h2 className="part-of-speech">{meaning["partOfSpeech"]}</h2>
+                                            <ol className="definitions">
+                                                {meaning["definitions"].map((definition) => {
+                                                    return (
+                                                        <div className="definition">
+                                                            {
+                                                                definition["definition"] &&
+                                                                <li>
+                                                                    <p className="definition">{definition["definition"]}</p>
+                                                                </li>
+                                                            }
+                                                            {
+                                                                definition["example"] &&
+                                                                <i className="example">Ex: {definition["example"]}</i>
+                                                            }
+                                                            {
+                                                                definition["synonyms"] &&
+                                                                <div><p>Synonyms:</p>
+                                                                    <ul className="synonyms">
+                                                                        {definition["synonyms"].map((synonym) => {
+                                                                            return (
+                                                                                <li className="synonym">{synonym}</li>
+                                                                            )
+                                                                        })}
+                                                                    </ul>
+                                                                </div>
+                                                            }
+                                                        </div>
+                                                    )
+                                                })}
+                                            </ol>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    }
+                </div>
+            )
+        } catch {
+            return (
+                <div>
+                    <div className="error-div">
+                        <p className="word-error">Invalid word</p>
+                    </div>
+                    <form onSubmit={this.fetchDictionary} id="dictionary-search">
+                        <input className="query" type="text" name="query" placeholder="Enter word..." />
+                        <input className="submit" type="submit" placeholder="Submit!" />
+                    </form>
+                </div>
+            )
+        }
+    }
 }
 
 export default App;
